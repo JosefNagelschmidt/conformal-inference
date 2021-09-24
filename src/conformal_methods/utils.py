ad12e86cda087b0b08f16da8ce5793977b5c8fc2
JosefNagelschmidt <achill95@web.de>
2021-09-24 02:14:06 +0200

    Added an example for usage

--- src/conformal_methods/utils.py
+++ src/conformal_methods/utils.py
@@ -12,13 +12,10 @@ from sklearn.preprocessing import StandardScaler
 def init_scoring_object(method, quantile=0.9):
     def scoring_object(estimator, X, y):
         if (method == "mean-based") | (method == "weighted-mean-based"):
-            #mu_hat = estimator.fit(X, y)
-            #y_pred = mu_hat.predict(X)
             y_pred = estimator.predict(X)
             loss = np.mean((y - y_pred)**2)
             return -loss.item()
         if method == "quantile-based":
-            #cond_quantile_hat = estimator.fit(X, y)
             y_pred = estimator.predict(X)
             if (quantile > 0) and (quantile < 1):
                 residual = y - y_pred 
